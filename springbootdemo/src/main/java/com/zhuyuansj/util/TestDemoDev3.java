ad012b44eb513b64b38187cf268f3f68947ccef1
shimilysj <124954495@qq.com>
2017-12-27 20:56:06 +0800

    新建分支feature1，在feature1上提交TestDemoDev3

--- springbootdemo/src/main/java/com/zhuyuansj/util/TestDemoDev3.java
+++ springbootdemo/src/main/java/com/zhuyuansj/util/TestDemoDev3.java
@@ -3,5 +3,6 @@ package com.zhuyuansj.util;
 public class TestDemoDev3 {
     public static void main(String[] args) {
         System.out.println("我是dev分支20171227");
+        System.out.println("Creating a new branch is quick AND simple");
     }
 }
